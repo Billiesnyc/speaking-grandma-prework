2a004d151e3cd433e9daa08c6d63908aff52b73b
Billie Simmons <billiesnyc@gmail.com>
2018-08-09 06:06:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,14 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma
+  if speak_to_grandma == speak_to_grandma.upcase
+    puts "NO, NOT SINCE 1938!"
+  end
+  elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
+else 
+  puts "HUH?! SPEAK UP,SONNY!"
+end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
